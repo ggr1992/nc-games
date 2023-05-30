609bc7e21fb0025f80c9f478052b75c5761e7ee1
ggr1992 <ggr92@hotmail.co.uk>
2023-05-30 13:31:28 +0100

    Completed Ticket 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import './App.css'
 import Header from './Components/Header'
 import Nav from './Components/Nav'
 import Reviews from './Components/Reviews'
+import { useState } from 'react'
 
 
 function App() {
+  const [reviews,setReviews] = useState([])
 
   return (
     <>
     <Nav/>
     <Header/>
-    <Reviews/>
+    <Reviews reviews={reviews} setReviews={setReviews}/>
     </>
   )
 }
---
+++ src/Components/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+
+const reviewApi = axios.create({
+    baseURL: "https://nc-games-project-evyy.onrender.com",
+  });
+
+export const fetchReviews = () => {
+    return reviewApi
+      .get("/api/reviews")
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => console.log(err));
+  }
