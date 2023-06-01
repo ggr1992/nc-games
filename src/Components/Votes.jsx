df5fb456dee4128d69e4a2038ad8949eb489a138
ggr1992 <ggr92@hotmail.co.uk>
2023-06-01 11:42:46 +0100

    Removed not needed file

--- src/Components/Votes.jsx
+++
@@ -1,4 +0,0 @@
-// We are going to need 2 states for votes one for comments and one for the review 
-// These should be placed in the single review and the comments files 
-// Will need to grab a thumbs up and thumbs down and have them increase and decrease the votes depending on what was pressed
-// Will need to figure out how to seperate the state for each comment as there are multiple on one page
